776e1e49fd6205a42c819ef029994c3faa3d7b35
40Sabrina <sabrinaaaaa@hotmail.com>
2023-12-14 14:06:52 +0100

    State.2 ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,40 @@
-import PokemonCard from "./components/PokemonCard"
-import NavBar from "./components/NavBar";
 import { useState } from "react";
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "charmander",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-  },
-  {
-    name: "squirtle",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-  },
-  {
-    name: "pikachu",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
+import NavBar from "./components/NavBar";
+import PokemonCard from "./components/PokemonCard"
 
 
 function App() {
-
   const [pokemonIndex, setPokemonIndex] = useState(0);
-  const handleClickPrécédent = () => {
-    if (pokemonIndex > 0) {
-      setPokemonIndex(pokemonIndex - 1)
-    }
-  }
-  const handleClickSuivant = () => {
-    if (pokemonIndex < pokemonList.length - 1) {
-      setPokemonIndex(pokemonIndex + 1)
-    }
-  }
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
   return (<>
     <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-    <NavBar handleClickPrécédent={handleClickPrécédent} handleClickSuivant={handleClickSuivant} />
-    {/* <button onClick={handleClickPrécédent}>Précédent</button>
-    <button onClick={handleClickSuivant}>Suivant</button> */}
+    <NavBar pokemonIndex={pokemonIndex} setPokemonIndex={setPokemonIndex} pokemonList={pokemonList} />
   </>)
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,22 @@
 
+function NavBar({ pokemonIndex, setPokemonIndex, pokemonList }) {
+    const handleClickPrecedent = () => {
+        if (pokemonIndex > 0) {
+            setPokemonIndex(pokemonIndex - 1)
+        }
+    }
+    const handleClickSuivant = () => {
+        if (pokemonIndex < pokemonList.length - 1) {
+            setPokemonIndex(pokemonIndex + 1)
+        }
+    }
 
-function NavBar({ handleClickPrécédent, handleClickSuivant }) {
-    return (<>
-        <button onClick={handleClickPrécédent}>Précédent</button>
-        <button onClick={handleClickSuivant}>Suivant</button>
-    </>);
+    return (
+        <>
+            <button onClick={handleClickPrecedent}>Précédent</button>
+            <button onClick={handleClickSuivant}>Suivant</button>
+        </>
+    )
 }
 
 
